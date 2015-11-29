3d8c365490cedcee82dcf509db98e8332fc05290
Justin Perez <chewymoonleaguesharp@gmail.com>
2015-11-29 14:39:11 -0500

    Jungle Tracker soon

---
+++ ElUtilitySuite/ElUtilitySuite/JungleTracker.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using LeagueSharp;
+using LeagueSharp.Common;
+using SharpDX;
+
+namespace ElUtilitySuite
+{
+    internal class JungleTracker
+    {
+        public static List<JungleCamp> JungleCamps { get; set; }
+         
+        static JungleTracker()
+        {
+            JungleCamps = new List<JungleCamp>();
+        }
+
+        public static void Init()
+        {
+            
+        }
+
+        public class JungleCamp
+        {
+            public JungleCamp(
+                float respawnTime,
+                Vector3 position,
+                string[] mobnames,
+                Utility.Map.MapType mapType)
+            {
+                RespawnTime = respawnTime;
+                Position = position;
+                MobNames = mobnames;
+                MapType = mapType;
+            }
+
+            public float RespawnTime { get; private set; }
+            public Vector3 Position { get; }
+
+            public Vector2 MinimapPosition
+            {
+                get { return Drawing.WorldToMinimap(Position); }
+            }
+
+            public string[] MobNames { get; set; }
+            public Utility.Map.MapType MapType { get; set; }
+        }
+    }
+}
