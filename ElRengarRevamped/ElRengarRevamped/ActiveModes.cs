e1b0ac6709a836bdd723d3d59d7e5e6979cb7d28
jQuery <info@zavox.nl>
2015-12-28 13:29:05 +0100

    xxx

--- ElRengarRevamped/ElRengarRevamped/ActiveModes.cs
+++ ElRengarRevamped/ElRengarRevamped/ActiveModes.cs
@@ -66,6 +66,7 @@ namespace ElRengarRevamped
                                             LastSwitch = Utils.GameTimeTickCount;
                                         }
                                     }
+                                    return;
                                 }
                             }
                             break;
