5f5cf6fdd3fc03d074c58c4f7da6c781a53b294b
jQuery <info@zavox.nl>
2016-01-02 12:46:43 +0100

    stuff

--- ElUtilitySuite/ElUtilitySuite/Summoners/Ignite.cs
+++ ElUtilitySuite/ElUtilitySuite/Summoners/Ignite.cs
@@ -16,7 +16,8 @@
             {
                 return !hero.HasBuff("summonerdot") || !hero.HasBuff("summonerbarrier") || !hero.HasBuff("BlackShield")
                        || !hero.HasBuff("SivirShield") || !hero.HasBuff("BansheesVeil")
-                       || !hero.HasBuff("ShroudofDarkness");
+                       || !hero.HasBuff("ShroudofDarkness") || !hero.HasBuffOfType(BuffType.SpellShield) || !hero.HasBuffOfType(BuffType.SpellImmunity)
+                   || !hero.HasBuffOfType(BuffType.Invulnerability);
             }
 
             return true;
