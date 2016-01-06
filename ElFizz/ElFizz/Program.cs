4f2fb3aa671a44d4b9fb65da3635cfc6c51b22f5
jQuery <info@zavox.nl>
2016-01-06 06:14:50 +0100

    Fizz release

---
+++ ElFizz/ElFizz/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ElFizz
+{
+    using LeagueSharp.Common;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Fizz.OnGameLoad;
+        }
+    }
+}
