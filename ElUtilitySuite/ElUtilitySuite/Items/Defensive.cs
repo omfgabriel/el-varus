60e1dbdc189f2d2fad89785a9c004c522abb2f9d
Justin Perez <chewymoonleaguesharp@gmail.com>
2016-01-15 21:25:18 -0500

    Offensive + Defensive cleanup (redo soon)

--- ElUtilitySuite/ElUtilitySuite/Items/Defensive.cs
+++ ElUtilitySuite/ElUtilitySuite/Items/Defensive.cs
@@ -32,7 +32,9 @@
 
         public static Obj_AI_Hero AggroTarget;
 
-        private static float incomingDamage, minionDamage;
+        private static float incomingDamage;
+
+        private static float minionDamage;
 
         #endregion
 
@@ -41,6 +43,8 @@
         public Menu Menu { get; set; }
 
         #endregion
+
+        #region Properties
 
         /// <summary>
         ///     Gets the player.
@@ -56,22 +60,9 @@
             }
         }
 
+        #endregion
+
         #region Public Methods and Operators
-
-        private Obj_AI_Hero Allies()
-        {
-            var target = this.Player;
-            foreach (var unit in
-                ObjectManager.Get<Obj_AI_Hero>()
-                    .Where(x => x.IsAlly && x.IsValidTarget(900, false))
-                    .OrderByDescending(xe => xe.Health / xe.MaxHealth * 100))
-            {
-                target = unit;
-            }
-
-            return target;
-        }
-
 
         /// <summary>
         ///     Creates the menu.
@@ -125,58 +116,18 @@
 
         #region Methods
 
-        private void OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
+        private Obj_AI_Hero Allies()
         {
-            if (sender.Type == GameObjectType.obj_AI_Hero && sender.IsEnemy)
+            var target = this.Player;
+            foreach (var unit in
+                ObjectManager.Get<Obj_AI_Hero>()
+                    .Where(x => x.IsAlly && x.IsValidTarget(900, false))
+                    .OrderByDescending(xe => xe.Health / xe.MaxHealth * 100))
             {
-                var heroSender = ObjectManager.Get<Obj_AI_Hero>().First(x => x.NetworkId == sender.NetworkId);
-                if (heroSender.GetSpellSlot(args.SData.Name) == SpellSlot.Unknown
-                    && args.Target.Type == Entry.Player.Type)
-                {
-                    AggroTarget = ObjectManager.GetUnitByNetworkId<Obj_AI_Hero>(args.Target.NetworkId);
-                    incomingDamage = (float)heroSender.GetAutoAttackDamage(AggroTarget);
-                }
-
-                if (heroSender.ChampionName == "Jinx" && args.SData.Name.Contains("JinxQAttack")
-                    && args.Target.Type == Entry.Player.Type)
-                {
-                    AggroTarget = ObjectManager.GetUnitByNetworkId<Obj_AI_Hero>(args.Target.NetworkId);
-                    incomingDamage = (float)heroSender.GetAutoAttackDamage(AggroTarget);
-                }
+                target = unit;
             }
 
-            if (sender.Type == GameObjectType.obj_AI_Minion && sender.IsEnemy)
-            {
-                if (args.Target.NetworkId == Entry.Player.NetworkId)
-                {
-                    AggroTarget = ObjectManager.GetUnitByNetworkId<Obj_AI_Hero>(args.Target.NetworkId);
-
-                    minionDamage =
-                        (float)
-                        sender.CalcDamage(
-                            AggroTarget,
-                            Damage.DamageType.Physical,
-                            sender.BaseAttackDamage + sender.FlatPhysicalDamageMod);
-                }
-            }
-
-            if (sender.Type == GameObjectType.obj_AI_Turret && sender.IsEnemy)
-            {
-                if (args.Target.Type == Entry.Player.Type)
-                {
-                    if (sender.Distance(this.Allies().ServerPosition, true) <= 900 * 900)
-                    {
-                        AggroTarget = ObjectManager.GetUnitByNetworkId<Obj_AI_Hero>(args.Target.NetworkId);
-
-                        incomingDamage =
-                            (float)
-                            sender.CalcDamage(
-                                AggroTarget,
-                                Damage.DamageType.Physical,
-                                sender.BaseAttackDamage + sender.FlatPhysicalDamageMod);
-                    }
-                }
-            }
+            return target;
         }
 
         private void CreateDefensiveItem(string displayname, string name, string type, int hpvalue, int dmgvalue)
@@ -210,41 +161,93 @@
             this.UseItem("allyshieldmountain", "Mountain", 3401, 700f);
             this.UseItem("selfshieldseraph", "Seraphs", 3040);
 
-            if (Items.HasItem(3069) && Items.CanUseItem(3069) && this.Menu.Item("useTalisman").GetValue<bool>())
+            if (!Items.HasItem(3069) || !Items.CanUseItem(3069) || !this.Menu.Item("useTalisman").GetValue<bool>())
             {
-                if (!Entry.Menu.Item("usecombo").GetValue<KeyBind>().Active
-                    && this.Menu.Item("talismanMode").GetValue<StringList>().SelectedIndex == 1)
+                return;
+            }
+
+            if (!Entry.Menu.Item("usecombo").GetValue<KeyBind>().Active
+                && this.Menu.Item("talismanMode").GetValue<StringList>().SelectedIndex == 1)
+            {
+                return;
+            }
+
+            var target = this.Allies();
+            if (target.Distance(Entry.Player.ServerPosition, true) > 600 * 600)
+            {
+                return;
+            }
+
+            var lowTarget =
+                ObjectManager.Get<Obj_AI_Hero>()
+                    .OrderBy(ex => ex.Health / ex.MaxHealth * 100)
+                    .First(x => x.IsValidTarget(1000));
+
+            var aHealthPercent = target.Health / target.MaxHealth * 100;
+            var eHealthPercent = lowTarget.Health / lowTarget.MaxHealth * 100;
+
+            if (lowTarget.Distance(target.ServerPosition, true) <= 900 * 900
+                && target.CountHerosInRange(false) > target.CountHerosInRange(true)
+                && eHealthPercent <= this.Menu.Item("useEnemyPct").GetValue<Slider>().Value)
+            {
+                Items.UseItem(3069);
+            }
+
+            if (target.CountHerosInRange(false) > target.CountHerosInRange(true)
+                && aHealthPercent <= this.Menu.Item("useAllyPct").GetValue<Slider>().Value)
+            {
+                Items.UseItem(3069);
+            }
+        }
+
+        private void OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
+        {
+            if (sender.Type == GameObjectType.obj_AI_Hero && sender.IsEnemy)
+            {
+                var heroSender = ObjectManager.Get<Obj_AI_Hero>().First(x => x.NetworkId == sender.NetworkId);
+                if (heroSender.GetSpellSlot(args.SData.Name) == SpellSlot.Unknown
+                    && args.Target.Type == Entry.Player.Type)
                 {
-                    return;
+                    AggroTarget = ObjectManager.GetUnitByNetworkId<Obj_AI_Hero>(args.Target.NetworkId);
+                    incomingDamage = (float)heroSender.GetAutoAttackDamage(AggroTarget);
                 }
 
-                var target = this.Allies();
-                if (target.Distance(Entry.Player.ServerPosition, true) > 600 * 600)
+                if (heroSender.ChampionName == "Jinx" && args.SData.Name.Contains("JinxQAttack")
+                    && args.Target.Type == Entry.Player.Type)
                 {
-                    return;
-                }
-
-                var lowTarget =
-                    ObjectManager.Get<Obj_AI_Hero>()
-                        .OrderBy(ex => ex.Health / ex.MaxHealth * 100)
-                        .First(x => x.IsValidTarget(1000));
-
-                var aHealthPercent = target.Health / target.MaxHealth * 100;
-                var eHealthPercent = lowTarget.Health / lowTarget.MaxHealth * 100;
-
-                if (lowTarget.Distance(target.ServerPosition, true) <= 900 * 900
-                    && (target.CountHerosInRange(false) > target.CountHerosInRange(true)
-                        && eHealthPercent <= this.Menu.Item("useEnemyPct").GetValue<Slider>().Value))
-                {
-                    Items.UseItem(3069);
-                }
-
-                if (target.CountHerosInRange(false) > target.CountHerosInRange(true)
-                    && aHealthPercent <= this.Menu.Item("useAllyPct").GetValue<Slider>().Value)
-                {
-                    Items.UseItem(3069);
+                    AggroTarget = ObjectManager.GetUnitByNetworkId<Obj_AI_Hero>(args.Target.NetworkId);
+                    incomingDamage = (float)heroSender.GetAutoAttackDamage(AggroTarget);
                 }
             }
+
+            if (sender.Type == GameObjectType.obj_AI_Minion && sender.IsEnemy
+                && (args.Target.NetworkId == Entry.Player.NetworkId))
+            {
+                AggroTarget = ObjectManager.GetUnitByNetworkId<Obj_AI_Hero>(args.Target.NetworkId);
+
+                minionDamage =
+                    (float)
+                    sender.CalcDamage(
+                        AggroTarget,
+                        Damage.DamageType.Physical,
+                        sender.BaseAttackDamage + sender.FlatPhysicalDamageMod);
+            }
+
+            if (!(sender.Distance(this.Allies().ServerPosition, true) <= 900 * 900)
+                || (args.Target.Type != Entry.Player.Type) || sender.Type != GameObjectType.obj_AI_Turret
+                || !sender.IsEnemy)
+            {
+                return;
+            }
+
+            AggroTarget = ObjectManager.GetUnitByNetworkId<Obj_AI_Hero>(args.Target.NetworkId);
+
+            incomingDamage =
+                (float)
+                sender.CalcDamage(
+                    AggroTarget,
+                    Damage.DamageType.Physical,
+                    sender.BaseAttackDamage + sender.FlatPhysicalDamageMod);
         }
 
         private void OnUpdate(EventArgs args)
@@ -287,7 +290,7 @@
                 return;
             }
 
-            var aHealthPercent = (int)((target.Health / target.MaxHealth) * 100);
+            var aHealthPercent = (int)(target.Health / target.MaxHealth * 100);
             var iDamagePercent = (int)(incomingDamage / target.MaxHealth * 100);
 
             if (!this.Menu.Item("DefenseOn" + target.CharData.BaseSkinName).GetValue<bool>())
@@ -295,26 +298,29 @@
                 return;
             }
 
-            if (menuvar.Contains("shield"))
+            if (!menuvar.Contains("shield"))
             {
-                if (aHealthPercent <= this.Menu.Item("use" + name + "Pct").GetValue<Slider>().Value)
-                {
-                    if ((iDamagePercent >= 1 || incomingDamage >= target.Health))
-                    {
-                        if (AggroTarget.NetworkId == target.NetworkId)
-                        {
-                            Items.UseItem(itemId, target);
-                        }
-                    }
+                return;
+            }
 
-                    if (iDamagePercent >= this.Menu.Item("use" + name + "Dmg").GetValue<Slider>().Value)
-                    {
-                        if (AggroTarget.NetworkId == target.NetworkId)
-                        {
-                            Items.UseItem(itemId, target);
-                        }
-                    }
-                }
+            if (aHealthPercent > this.Menu.Item("use" + name + "Pct").GetValue<Slider>().Value)
+            {
+                return;
+            }
+
+            if ((iDamagePercent >= 1 || incomingDamage >= target.Health) && (AggroTarget.NetworkId == target.NetworkId))
+            {
+                Items.UseItem(itemId, target);
+            }
+
+            if (iDamagePercent < this.Menu.Item("use" + name + "Dmg").GetValue<Slider>().Value)
+            {
+                return;
+            }
+
+            if (AggroTarget.NetworkId == target.NetworkId)
+            {
+                Items.UseItem(itemId, target);
             }
         }
 
@@ -330,13 +336,15 @@
                 return;
             }
 
-            if (this.Menu.Item("use" + name).GetValue<bool>())
+            if (!this.Menu.Item("use" + name).GetValue<bool>())
             {
-                if (Entry.Player.CountHerosInRange(true, itemRange)
-                    >= this.Menu.Item("use" + name + "Count").GetValue<Slider>().Value)
-                {
-                    Items.UseItem(itemId);
-                }
+                return;
+            }
+
+            if (Entry.Player.CountHerosInRange(true, itemRange)
+                >= this.Menu.Item("use" + name + "Count").GetValue<Slider>().Value)
+            {
+                Items.UseItem(itemId);
             }
         }
 
--- ElUtilitySuite/ElUtilitySuite/Items/Offensive.cs
+++ ElUtilitySuite/ElUtilitySuite/Items/Offensive.cs
@@ -94,18 +94,20 @@
 
         private void Obj_AI_Base_OnProcessSpellCast(Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
         {
-            if (sender.IsMe && (Items.HasItem(3042) || Items.HasItem(3043)))
+            if (!sender.IsMe || (!Items.HasItem(3042) && !Items.HasItem(3043)))
             {
-                if (Entry.Player.GetSpellSlot(args.SData.Name) == SpellSlot.Unknown
-                    && (this.Menu.Item("usecombo").GetValue<KeyBind>().Active || args.Target.Type == Entry.Player.Type))
-                {
-                    CanManamune = true;
-                }
+                return;
+            }
 
-                else
-                {
-                    Utility.DelayAction.Add(400, () => CanManamune = false);
-                }
+            if (Entry.Player.GetSpellSlot(args.SData.Name) == SpellSlot.Unknown
+                && (this.Menu.Item("usecombo").GetValue<KeyBind>().Active || args.Target.Type == Entry.Player.Type))
+            {
+                CanManamune = true;
+            }
+
+            else
+            {
+                Utility.DelayAction.Add(400, () => CanManamune = false);
             }
         }
 
@@ -142,17 +144,19 @@
                 }
             }
 
-            if (Entry.Menu.Item("usecombo").GetValue<KeyBind>().Active)
+            if (!Entry.Menu.Item("usecombo").GetValue<KeyBind>().Active)
             {
-                this.UseItem("Frostclaim", 3092, 850f, true);
-                this.UseItem("Youmuus", 3142, 650f);
-                this.UseItem("Hydra", 3077, 250f);
-                this.UseItem("Hydra", 3074, 250f);
-                this.UseItem("Hextech", 3146, 700f, true);
-                this.UseItem("Cutlass", 3144, 450f, true);
-                this.UseItem("Botrk", 3153, 450f, true);
-                this.UseItem("Titanic", 3748, 450f);
+                return;
             }
+
+            this.UseItem("Frostclaim", 3092, 850f, true);
+            this.UseItem("Youmuus", 3142, 650f);
+            this.UseItem("Hydra", 3077, 250f);
+            this.UseItem("Hydra", 3074, 250f);
+            this.UseItem("Hextech", 3146, 700f, true);
+            this.UseItem("Cutlass", 3144, 450f, true);
+            this.UseItem("Botrk", 3153, 450f, true);
+            this.UseItem("Titanic", 3748, 450f);
         }
 
         private void OnUpdate(EventArgs args)
@@ -194,55 +198,59 @@
                 target = targ;
             }
 
-            if (target.IsValidTarget(range))
+            if (!target.IsValidTarget(range))
             {
-                if (target != null)
+                return;
+            }
+
+            if (target == null)
+            {
+                return;
+            }
+
+            var eHealthPercent = (int)((target.Health / target.MaxHealth) * 100);
+            var aHealthPercent = (int)((Entry.Player.Health / target.MaxHealth) * 100);
+
+            if (eHealthPercent <= this.Menu.Item("use" + name + "Pct").GetValue<Slider>().Value
+                && this.Menu.Item("ouseOn" + target.CharData.BaseSkinName).GetValue<bool>())
+            {
+                if (targeted && itemId == 3092)
                 {
-                    var eHealthPercent = (int)((target.Health / target.MaxHealth) * 100);
-                    var aHealthPercent = (int)((Entry.Player.Health / target.MaxHealth) * 100);
+                    var pi = new PredictionInput
+                                 {
+                                     Aoe = true, Collision = false, Delay = 0.0f, From = Entry.Player.Position,
+                                     Radius = 250f, Range = 850f, Speed = 1500f, Unit = target,
+                                     Type = SkillshotType.SkillshotCircle
+                                 };
 
-                    if (eHealthPercent <= this.Menu.Item("use" + name + "Pct").GetValue<Slider>().Value
-                        && this.Menu.Item("ouseOn" + target.CharData.BaseSkinName).GetValue<bool>())
+                    var po = Prediction.GetPrediction(pi);
+                    if (po.Hitchance >= HitChance.Medium)
                     {
-                        if (targeted && itemId == 3092)
-                        {
-                            var pi = new PredictionInput
-                                         {
-                                             Aoe = true, Collision = false, Delay = 0.0f, From = Entry.Player.Position,
-                                             Radius = 250f, Range = 850f, Speed = 1500f, Unit = target,
-                                             Type = SkillshotType.SkillshotCircle
-                                         };
-
-                            var po = Prediction.GetPrediction(pi);
-                            if (po.Hitchance >= HitChance.Medium)
-                            {
-                                Items.UseItem(itemId, po.CastPosition);
-                            }
-                        }
-
-                        else if (targeted)
-                        {
-                            Items.UseItem(itemId, target);
-                        }
-
-                        else
-                        {
-                            Items.UseItem(itemId);
-                        }
+                        Items.UseItem(itemId, po.CastPosition);
                     }
+                }
 
-                    else if (aHealthPercent <= this.Menu.Item("use" + name + "Me").GetValue<Slider>().Value
-                             && this.Menu.Item("ouseOn" + target.CharData.BaseSkinName).GetValue<bool>())
-                    {
-                        if (targeted)
-                        {
-                            Items.UseItem(itemId, target);
-                        }
-                        else
-                        {
-                            Items.UseItem(itemId);
-                        }
-                    }
+                else if (targeted)
+                {
+                    Items.UseItem(itemId, target);
+                }
+
+                else
+                {
+                    Items.UseItem(itemId);
+                }
+            }
+
+            else if (aHealthPercent <= this.Menu.Item("use" + name + "Me").GetValue<Slider>().Value
+                     && this.Menu.Item("ouseOn" + target.CharData.BaseSkinName).GetValue<bool>())
+            {
+                if (targeted)
+                {
+                    Items.UseItem(itemId, target);
+                }
+                else
+                {
+                    Items.UseItem(itemId);
                 }
             }
         }
