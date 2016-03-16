96de26495cb6bfc3ac4cb566d6b968bb675861c3
Nathan Sakoetoe <info@zavox.nl>
2016-03-17 00:46:18 +0100

    Update Program.cs

--- ElRumble/ElRumble/Program.cs
+++ ElRumble/ElRumble/Program.cs
@@ -13,7 +13,7 @@ namespace ElRumble
     {
         private static void Main(string[] args)
         {
-            CustomEvents.Game.OnGameLoad += BROKEN.OnLoad;
+            CustomEvents.Game.OnGameLoad += Rumble.OnLoad;
         }
     }
 }
