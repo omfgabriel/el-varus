6eb554b54910117b726e6fb12cd56d9b1d6b20b2
jQuery <info@zavox.nl>
2016-03-24 20:45:48 +0100

    gddgsdgsgds

---
+++ ElAurelion Sol/ElAurelion Sol/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ElAurelion_Sol
+{
+    using LeagueSharp.Common;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += AurelionSol.OnGameLoad;
+        }
+    }
+}
