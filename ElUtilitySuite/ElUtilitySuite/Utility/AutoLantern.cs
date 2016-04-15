155451ab778bf9d1b95fe335f153b9297f1272b3
jQuery <info@zavox.nl>
2016-04-15 22:35:34 +0200

    1.0.3.28

--- ElUtilitySuite/ElUtilitySuite/Utility/AutoLantern.cs
+++ ElUtilitySuite/ElUtilitySuite/Utility/AutoLantern.cs
@@ -1,21 +1,58 @@
 ﻿namespace ElUtilitySuite.Utility
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
 
     using LeagueSharp;
     using LeagueSharp.Common;
 
-    using ItemData = LeagueSharp.Common.Data.ItemData;
-
-    internal class AutoLantern 
+    internal class AutoLantern : IPlugin
     {
+        #region Fields
+
+        public GameObject ThreshLantern;
+
+        #endregion
+
         #region Public Properties
 
         public Menu Menu { get; set; }
 
         #endregion
+
+        #region Properties
+
+        /// <summary>
+        ///     Gets the click below hp menu value.
+        /// </summary>
+        /// <value>
+        ///     The lantern below hp menu value.
+        /// </value>
+        private int ClickBelowHp
+        {
+            get
+            {
+                return this.Menu.Item("ThreshLanternHPSlider").GetValue<Slider>().Value;
+            }
+        }
+
+        /// <summary>
+        ///     Gets the player.
+        /// </summary>
+        /// <value>
+        ///     The player.
+        /// </value>
+        private Obj_AI_Hero Player
+        {
+            get
+            {
+                return ObjectManager.Player;
+            }
+        }
+
+        #endregion
+
+        #region Public Methods and Operators
 
         /// <summary>
         ///     Creates the menu.
@@ -25,13 +62,17 @@
         public void CreateMenu(Menu rootMenu)
         {
             var predicate = new Func<Menu, bool>(x => x.Name == "MiscMenu");
-            var menu = !rootMenu.Children.Any(predicate) ? rootMenu.AddSubMenu(new Menu("Misc", "MiscMenu")) : rootMenu.Children.First(predicate);
+            var menu = !rootMenu.Children.Any(predicate)
+                           ? rootMenu.AddSubMenu(new Menu("Misc", "MiscMenu"))
+                           : rootMenu.Children.First(predicate);
 
-            var autoLanternMenu = menu.AddSubMenu(new Menu("Thresh lantern", "Threshlantern"));
+            var autoLanternMenu = menu.AddSubMenu(new Menu("Thresh Lantern", "Threshlantern"));
             {
                 autoLanternMenu.AddItem(new MenuItem("ThreshLantern", "Auto click Thresh lantern").SetValue(true));
-                autoLanternMenu.AddItem(new MenuItem("ThreshLantern", "Hotkey").SetValue(new KeyBind('M', KeyBindType.Press)));
-                autoLanternMenu.AddItem(new MenuItem("ThreshLantern.Health", "Click when HP %").SetValue(new Slider(20)));
+                autoLanternMenu.AddItem(
+                    new MenuItem("ThreshLanternHotkey", "Hotkey").SetValue(new KeyBind('M', KeyBindType.Press)));
+                autoLanternMenu.AddItem(
+                    new MenuItem("ThreshLanternHPSlider", "Click when HP %").SetValue(new Slider(20)));
             }
 
             this.Menu = autoLanternMenu;
@@ -42,7 +83,85 @@
         /// </summary>
         public void Load()
         {
-            
+            try
+            {
+                Game.OnUpdate += this.OnUpdate;
+                GameObject.OnCreate += this.OnCreate;
+                GameObject.OnDelete += this.OnDelete;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: '{0}'", e);
+            }
         }
+
+        #endregion
+
+        #region Methods
+
+        private void OnCreate(GameObject sender, EventArgs args)
+        {
+            try
+            {
+                if (!sender.IsValid || !sender.IsAlly || sender.Type != GameObjectType.obj_AI_Minion)
+                {
+                    return;
+                }
+                if (sender.Name.Equals("ThreshLantern", StringComparison.OrdinalIgnoreCase))
+                {
+                    this.ThreshLantern = sender;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: '{0}'", e);
+            }
+        }
+
+        private void OnDelete(GameObject sender, EventArgs args)
+        {
+            try
+            {
+                if (!sender.IsValid || this.ThreshLantern == null)
+                {
+                    return;
+                }
+
+                if (sender.NetworkId == this.ThreshLantern.NetworkId)
+                {
+                    this.ThreshLantern = null;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: '{0}'", e);
+            }
+        }
+
+        private void OnUpdate(EventArgs args)
+        {
+            try
+            {
+                if (this.Player.IsDead || !this.Menu.Item("ThreshLantern").IsActive())
+                {
+                    return;
+                }
+
+                if (this.Player.HealthPercent < this.ClickBelowHp
+                    || this.Menu.Item("ThreshLanternHotkey").GetValue<KeyBind>().Active)
+                {
+                    if (this.ThreshLantern.Position.Distance(this.Player.Position) <= 500)
+                    {
+                        this.Player.Spellbook.CastSpell((SpellSlot)62, this.ThreshLantern);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: '{0}'", e);
+            }
+        }
+
+        #endregion
     }
 }
