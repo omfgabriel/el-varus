41dd44b66f5e57c719078e9ffb85350b9617550d
YeezyYeezyWhatsGood <YeezyYeezy@RiotGamesSueMyCunt.com>
2016-09-04 18:28:44 +0200

    -60

--- ElSinged/ElSinged/ElSingedMenu.cs
+++ ElSinged/ElSinged/ElSingedMenu.cs
@@ -67,14 +67,6 @@ namespace ElSinged
             }
             Menu.AddSubMenu(miscMenu);
 
-            //Here comes the moneyyy, money, money, moneyyyy
-            var credits = new Menu("Credits", "jQuery");
-            {
-                credits.AddItem(new MenuItem("ElSinged.Paypal", "if you would like to donate via paypal:"));
-                credits.AddItem(new MenuItem("ElSinged.Email", "info@zavox.nl"));
-            }
-            Menu.AddSubMenu(credits);
-
             Menu.AddItem(new MenuItem("422442fsaafs4242f", ""));
             Menu.AddItem(new MenuItem("422442fsaafsf", "Version: 1.0.0.4"));
             Menu.AddItem(new MenuItem("fsasfafsfsafsa", "Made By jQuery"));
