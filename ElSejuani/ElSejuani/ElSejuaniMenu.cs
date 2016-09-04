d5d9323681decb4f263fe867f61566148c42c1e1
YeezyYeezyWhatsGood <YeezyYeezy@RiotGamesSueMyCunt.com>
2016-09-04 18:13:47 +0200

    -60

--- ElSejuani/ElSejuani/ElSejuaniMenu.cs
+++ ElSejuani/ElSejuani/ElSejuaniMenu.cs
@@ -82,12 +82,6 @@ namespace ElSejuani
 
             Menu.AddSubMenu(miscMenu);
 
-            //Here comes the moneyyy, money, money, moneyyyy
-            var credits = new Menu("Credits", "jQuery");
-            credits.AddItem(new MenuItem("ElCorki.Paypal", "if you would like to donate via paypal:"));
-            credits.AddItem(new MenuItem("ElCorki.Email", "info@zavox.nl"));
-            Menu.AddSubMenu(credits);
-
             Menu.AddItem(new MenuItem("422442fsaafs4242f", ""));
             Menu.AddItem(new MenuItem("422442fsaafsf", "Version: 1.0.0.1"));
             Menu.AddItem(new MenuItem("fsasfafsfsafsa", "Made By jQuery"));
