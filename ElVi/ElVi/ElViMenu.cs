9b71f4f8abfdc94da5538d982d185598d3056099
YeezyYeezyWhatsGood <YeezyYeezy@RiotGamesSueMyCunt.com>
2016-09-04 18:37:48 +0200

    -60

--- ElVi/ElVi/ElViMenu.cs
+++ ElVi/ElVi/ElViMenu.cs
@@ -118,14 +118,6 @@ namespace ElVi
 
             _menu.AddSubMenu(miscMenu);
 
-            //Here comes the moneyyy, money, money, moneyyyy
-            var credits = new Menu("Credits", "jQuery");
-            {
-                credits.AddItem(new MenuItem("ElVi.Paypal", "if you would like to donate via paypal:"));
-                credits.AddItem(new MenuItem("ElVi.Email", "info@zavox.nl"));
-            }
-            _menu.AddSubMenu(credits);
-
             _menu.AddItem(new MenuItem("422442fsaafs4242f", ""));
             _menu.AddItem(new MenuItem("422442fsaafsf", "Version: 1.0.0.1"));
             _menu.AddItem(new MenuItem("fsasfafsfsafsa", "Made By jQuery"));
